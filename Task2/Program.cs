d6a3e2c150ebe401b932d868dc7e7d006bbf974a
Serhii <serhii.soldatov@gmail.com>
2022-04-13 15:26:19 +0200

    Решение задачи №4

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a, b, c;
+a = 10;
+b = 10;
+c = 10;
+if (a > b && a > c)
+{
+    System.Console.WriteLine($"Число {a} максимальное.");
+}
+else if (b > a && b > c)
+{
+    System.Console.WriteLine($"Число {b} максимальное.");
+} 
+else if (c > a && c > b)
+{
+    System.Console.WriteLine($"Число {c} максимальное.");
+}
+else if (a == b && b == c && c == a)
+{
+    System.Console.WriteLine($"Все числа равнозначны");
+}
